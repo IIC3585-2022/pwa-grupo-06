abf5ca07cfde8b055736975c577a65ee7d521a15
perezpefaur <perezpefaur@uc.cl>
2022-05-04 13:56:39 -0400

    debugg

--- js/index.js
+++ js/index.js
@@ -18,7 +18,7 @@ const firebaseConfig = {
 };
 
 if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("sw.js").then(registration => {
+    navigator.serviceWorker.register("firebase-messaging-sw.js").then(registration => {
         console.log(registration);
     }).catch(error => {
         console.error(error);
