be62b24d1026b04fa2b0b4c424c42b6905ba2fde
perezpefaur <perezpefaur@uc.cl>
2022-05-04 10:44:08 -0400

    Ver si funciona ahora

--- js/create.js
+++ js/create.js
@@ -1,5 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.7.0/firebase-app.js'
+import { getAnalytics } from 'https://www.gstatic.com/firebasejs/9.7.0/firebase-analytics.js'
 import { getMessaging } from 'https://www.gstatic.com/firebasejs/9.7.0/firebase-messaging.js'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCiiDUincmDNXCFcAlVXxR0y6tZqMeclWU",
+  authDomain: "ctplmdc.firebaseapp.com",
+  projectId: "ctplmdc",
+  storageBucket: "ctplmdc.appspot.com",
+  messagingSenderId: "347639001430",
+  appId: "1:347639001430:web:76af4be10c775e5f81a6f7",
+  measurementId: "G-82XYRRQSDM"
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const messaging = getMessaging(app);
 
 function create() {
--- js/index.js
+++ js/index.js
@@ -1,21 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.7.0/firebase-app.js'
-import { getAnalytics } from 'https://www.gstatic.com/firebasejs/9.7.0/firebase-analytics.js'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCiiDUincmDNXCFcAlVXxR0y6tZqMeclWU",
-  authDomain: "ctplmdc.firebaseapp.com",
-  projectId: "ctplmdc",
-  storageBucket: "ctplmdc.appspot.com",
-  messagingSenderId: "347639001430",
-  appId: "1:347639001430:web:76af4be10c775e5f81a6f7",
-  measurementId: "G-82XYRRQSDM"
-};
-
 if ("serviceWorker" in navigator) {
     navigator.serviceWorker.register("sw.js").then(registration => {
         console.log(registration);
@@ -23,7 +5,3 @@ if ("serviceWorker" in navigator) {
         console.error(error);
     });
 }
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
