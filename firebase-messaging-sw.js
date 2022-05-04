34a1243a102e9a764e9aa5cf1052e0d367e779e8
perezpefaur <perezpefaur@uc.cl>
2022-05-04 13:43:22 -0400

    ver si llega notificacion

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,27 @@
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCiiDUincmDNXCFcAlVXxR0y6tZqMeclWU",
+    authDomain: "ctplmdc.firebaseapp.com",
+    projectId: "ctplmdc",
+    storageBucket: "ctplmdc.appspot.com",
+    messagingSenderId: "347639001430",
+    appId: "1:347639001430:web:76af4be10c775e5f81a6f7",
+    measurementId: "G-82XYRRQSDM"
+  };
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    // Customize notification here
+    const notificationTitle = 'Background Message Title';
+    const notificationOptions = {
+      body: 'Background Message body.',
+      icon: '/firebase-logo.png'
+    };
+  
+    self.registration.showNotification(notificationTitle,
+      notificationOptions);
+  });
