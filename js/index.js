926b7bfa23856fc15192a0c63ccbb4ad93fe0235
perezpefaur <perezpefaur@uc.cl>
2022-05-04 10:24:19 -0400

    debugg

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getMessaging } from "firebase/messaging";
+import { initializeApp } from "./node_modules/firebase/app";
+import { getAnalytics } from "./node_modules/firebase/analytics";
+import { getMessaging } from "./node_modules/firebase/messaging";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
